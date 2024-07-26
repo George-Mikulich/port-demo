c4ce1d90d8b6b544f35b1682be9b304215591695
George-Mikulich <g.mikulich@softteco.com>
2024-07-26 18:00:26 +0300

    fix destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -14,6 +14,10 @@ kubectl get providers -oname | xargs kubectl delete
 helm uninstall crossplane --namespace crossplane-system
 kubectl delete namespace crossplane-system
 
+kubectl get providerconfigs -oname | xargs kubectl delete
+kubectl get locks.pkg.crossplane.io -oname | xargs kubectl delete
+kubectl get providerrevisions -oname | xargs kubectl delete
+
 kubectl get crd -oname | grep --color=never 'upbound.io' | xargs kubectl delete
 kubectl get crd -oname | grep --color=never 'crossplane.io' | xargs kubectl delete
 
